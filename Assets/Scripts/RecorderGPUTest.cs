87e9efbe7cc51f5e4a007372ff4a7f391e2dc84d
Yusuke Kurokawa <wotakuro@gmail.com>
2020-11-25 09:33:17 +0900

    update

--- Assets/Scripts/RecorderGPUTest.cs
+++ Assets/Scripts/RecorderGPUTest.cs
@@ -2,20 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Profiling;
 
 public class RecorderGPUTest : MonoBehaviour
 {
     [SerializeField]
     private Text isSupportGpuRecorder;
+
+    private Recorder recordCameraRender;
+
     // Start is called before the first frame update
     void Start()
     {
         isSupportGpuRecorder.text = "supportGPURecorder:"+SystemInfo.supportsGpuRecorder;
+
+        recordCameraRender = Recorder.Get("Camera.Render");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(
+        recordCameraRender.gpuElapsedNanoseconds);
     }
 }
